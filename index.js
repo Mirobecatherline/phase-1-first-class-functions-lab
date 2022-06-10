bd26cd2d3aee2bcef20e72c27e93880fe9dc61a5
catherline nyanduko mirobe <catherline.nyanduko@student.moringaschool.com>
2022-06-10 11:36:03 +0300

    commit tree 47e322f2dc826996e575c2380813ff9fddfcabd1

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers=function (drivers) {
+    return drivers.slice(0,2);
+}
+const returnLastTwoDrivers=function (drivers) {
+    return drivers.slice(2);
+}
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+function createFareMultiplier(x){
+    return function () {
+        return x*5;
+    }
+} 
+const fareDoubler=function (createFareMultiplier) {
+    return 2*createFareMultiplier;
+}
+const fareTripler=function (createFareMultiplier) {
+    return 3*createFareMultiplier;
+}
+function selectDifferentDrivers(drivers,returnLastTwoDrivers){
+    return returnLastTwoDrivers(drivers);
+}
